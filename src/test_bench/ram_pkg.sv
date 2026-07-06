3ebe7496c687ab0c1d9adb82a13607921208cd48
Vijay-001-Vlsi <vijaym08m@gmail.com>
2026-07-06 22:26:25 +0530

    Create ram_pkg.sv

---
+++ src/test_bench/ram_pkg.sv
@@ -0,0 +1,12 @@
+package ram_pkg;
+
+    `include "transaction.sv"
+    `include "generator.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "ref.sv"
+   `include "scoreboard.sv"
+    `include "env.sv"
+    `include "test.sv"
+
+endpackage
